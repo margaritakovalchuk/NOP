83e1f7df09560bd1f44a14a389793f46518c8b4b
margaritakovalchuk <margoshakovalchyk@gmail.com>
2019-11-14 18:35:33 +0200

    Done first page

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -12,6 +12,7 @@ import babel from 'gulp-babel';
 import concat from 'gulp-concat';
 import uglify from 'gulp-uglify';
 import rename from 'gulp-rename';
+import svgSprite from 'gulp-svg-sprite';
 
 const path = {
     baseDir: './dist',
@@ -91,7 +92,7 @@ const js = () => {
             suffix: '.min',
         }))
         .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest(path.js.dist))
+        // .pipe(gulp.dest(path.js.dist))
         .pipe(browserSync.stream());
 };
 
@@ -107,10 +108,32 @@ const sync = () => {
     gulp.watch(`${path.html.dist}/**/*.html`).on('change', browserSync.reload);
 };
 
-const minimizeImages = () => {
+const images = () => {
     return gulp
-        .src(`${path.img.src}/**/*.{png,jpg,gif,jpeg}`)
+        .src(`${path.img.src}/**/*.{png,jpg,gif,jpeg,webp}`)
         // .pipe(imgmin())
+        .pipe(gulp.dest(path.img.dist));
+};
+
+const spriteSvg = () => {
+    const config = {
+        mode: {
+            symbol: true,
+        },
+        shape: {
+            transform: ['svgo'],
+        },
+        svg: {
+            namespaceClassnames: true,
+        },
+    };
+
+    return gulp
+        .src(`${path.img.src}/**/*.svg`)
+        .pipe(svgSprite(config))
+        .pipe(rename({
+            basename: 'sprite'
+        }))
         .pipe(gulp.dest(path.img.dist));
 };
 
@@ -127,4 +150,4 @@ const fonts = () => {
         .pipe(gulp.dest(path.fonts.dist));
 };
 
-export default gulp.series(clean, minimizeImages, fonts, gulp.parallel(sass, js, html), sync);
+export default gulp.series(clean, images, spriteSvg, fonts, gulp.parallel(sass, js, html), sync);

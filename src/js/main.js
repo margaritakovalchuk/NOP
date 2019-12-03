cd5dd3f7bc96db5eee8eee8065add2aa9e2eef4d
margaritakovalchuk <margoshakovalchyk@gmail.com>
2019-12-03 14:18:39 +0200

    Search wrapper (work-in-progress)

--- src/js/main.js
+++ src/js/main.js
@@ -13,23 +13,25 @@ function init() {
         overlayClass: '.overlay',
     });
 
-    new Search();
+    new Search({
+        mainClass: '.main-content',
+        pageClass: '.page',
+        headerClass: '.header-wrapper',
+        menuClass: '.navigation-main',
+        menuButtonClass: '.nav-toggle',
+        mobileMenuClass: '.mobile-menu',
+        sidebarClass: '.sidebar-menu.search-sidebar',
+        search:'.navigation__search',
+        searchField: '.search-wrapper',
+        searchInput: '.search-wrapper input',
+        searchWrapperClass: '.search-sidebar__wrapper',
+        cross: '.menu-toggle__close',
+        overlayClass: '.overlay',
+    });
 
     new FilterArticles({
         wrapper: '.inner__filter',
         current: '.filter-current',
         list: '.filter-list-pulldown',
     });
-
-    // new SearchSidebar({
-    //     // mainClass: '.main-content',
-    //     // pageClass: '.page',
-    //     // headerClass: '.header-wrapper',
-    //     // menuClass: '.navigation-main',
-    //     searchButtonClass: '.navigation__search',
-    //     // sidebarClass: '.sidebar-menu',
-    //     // // mobileMenuClass: '.mobile-menu',
-    //     // cross: '.menu-toggle__close',
-    //     // overlayClass: '.overlay',
-    // });
 }
--- src/libs/js/search.js
+++ src/libs/js/search.js
@@ -1,20 +1,49 @@
-const Search = function() {
+const Search = ({sidebarClass, searchClass, searchFieldClass, searchInputClass,
+                    cross, overlayClass, mainClass, pageClass, headerClass, menuClass, menuButtonClass}) => {
     const body = document.body,
-        search = document.querySelector('.navigation__search'),
-        searchField = document.querySelector('.search-wrapper'),
-        searchInput = document.querySelector('.search-wrapper input'),
-        searchButton = document.querySelector('.search-wrapper .btn-search');
+        sidebar = document.querySelector(sidebarClass),
+        search = document.querySelector(searchClass),
+        searchField = document.querySelector(searchFieldClass),
+        searchInput = document.querySelector(searchInputClass),
+        close = document.querySelector(cross),
+        overlay = document.querySelector(overlayClass),
+        mainContent = document.querySelector(mainClass),
+        page = document.querySelector(pageClass),
+        header = document.querySelector(headerClass),
+        menu = document.querySelector(menuClass),
+        menuButton = document.querySelector(menuButtonClass);
 
     const toggleSearchField = () => {
-        if (searchField.classList.contains('d-block')) {
-            searchField.classList.remove('d-block');
-            searchField.classList.add('d-none');
-        } else {
-            searchField.classList.remove('d-none');
-            searchField.classList.add('d-block');
-            searchInput.focus();
-        }
-    };
 
-    search.addEventListener('click', toggleSearchField);
+        if (window.matchMedia("(max-width: 767px)").matches) {
+
+            if (sidebar.classList.add('sidebar-menu_active')) {
+                mainContent.classList.add('main-content_active');
+                header.classList.add('main-content_active');
+                page.style.overflow = 'hidden';
+                overlay.style.display = 'block';
+                menuButton.style.display = 'none';
+            } else {
+                sidebar.classList.remove('sidebar-menu_active');
+                mainContent.classList.remove('main-content_active');
+                header.classList.remove('main-content_active');
+                page.style.overflow = 'visible';
+                overlay.style.display = 'none';
+                menuButton.style.display = 'block';
+            }
+
+        } else {
+
+            if (searchField.classList.contains('d-block')) {
+                searchField.classList.remove('d-block');
+                searchField.classList.add('d-none');
+            } else {
+                searchField.classList.remove('d-none');
+                searchField.classList.add('d-block');
+                searchInput.focus();
+            }
+        }
+
+        search.addEventListener('click', toggleSearchField);
+    }
 };
